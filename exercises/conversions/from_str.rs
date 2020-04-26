351fc1cf11bb6ea600a9e604c1bd96f3c742da5b
Herbert Henrique <herbert.henrique@sumup.com>
2020-04-26 19:58:16 -0300

    Finish conversions exercises

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -21,6 +20,20 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err("input len is 0".to_string());
+        }
+
+        let mut parts = s.split(",").collect::<Vec<&str>>();
+
+        let ageResult = parts.pop().unwrap().parse::<usize>();
+
+        let name = parts.pop().unwrap();
+
+        return Ok(Person {
+            name: String::from(name),
+            age: ageResult.unwrap(),
+        });
     }
 }
 
